85dafb5324c6f88ef96339216c2ff77b4faf6548
Gaurav Arora <gauravarora.daiict@gmail.com>
2019-09-15 00:42:09 +1000

    Add changges

--- TestCMS.java
+++ TestCMS.java
@@ -8,31 +8,72 @@ public class TestCMS {
 		int width = Integer.parseInt(args[1]);
 		int length = Integer.parseInt(args[2]);
 		double skew = Double.parseDouble(args[3]);
-		double error = test_configuration(depth, width, length, skew);
-		System.out.println("Maximum error in stream is " + String.valueOf(error));
-	}
-
-	public static double test_configuration(int depth, int width, int length, double skew) {
 		Stream st = new Stream(length, skew);
-		CMS_default def = new CMS_default(depth, width);
+		/*CMS_default def = new CMS_default(depth, width);
 		CMS_conservative conv = new CMS_conservative(depth, width);
 		CMS_Morris morris = new CMS_Morris(depth, width);
+		double error = test_configuration(depth, width, length, skew, st, def);
+		System.out.println("Default Maximum error in stream is " + String.valueOf(error));
+		error = test_configuration(depth, width, length, skew, st, conv);
+		System.out.println("Conv Maximum error in stream is " + String.valueOf(error));
+		error = test_configuration(depth, width, length, skew, st, morris);
+		System.out.println("Morris Maximum error in stream is " + String.valueOf(error));
+		*/
+		run_skew_experiment(depth, width, length,  st, "default");
+		run_skew_experiment(depth, width, length,  st, "conservative");
+		run_skew_experiment(depth, width, length,  st, "morris");
+		run_width_experiment(depth, skew, length,  st, "default");
+		run_width_experiment(depth, skew, length,  st, "conservative");
+		run_width_experiment(depth, skew, length,  st, "morris");
+	}
+
+	public static CMS get_counter(String counter_name, int depth, int width) {
+		if (counter_name == "default") {
+			return new CMS_default(depth, width);
+		} else if (counter_name == "conv") {
+			return new CMS_conservative(depth, width);
+		} else if (counter_name == "morris") {
+			return new CMS_Morris(depth, width);
+		}
+		return new CMS_default(depth, width);
+	}
+
+	public static void run_width_experiment(int depth, double skew, int length, Stream st, String counter_name) {
+		int [] widths = {200, 2000, 20000, 200000, 2000000, 20000000};
+		for (int i = 0; i < widths.length; i++) {
+			CMS sketch = get_counter(counter_name, depth, widths[i]);
+			double error = test_configuration(depth, widths[i], length, skew, st, sketch);
+			System.out.println(counter_name + " Maximum error in stream is Width : "  + 
+					String.valueOf(widths[i]) + " is  "+ String.valueOf(error));
+		}
+	}
+
+	public static void run_skew_experiment(int depth, int width, int length, Stream st, String counter_name) {
+		double [] skews = {0, 0.2, 0.5, 0.8, 1, 1.2,1.5, 2,2.5,10 };
+		for (int i = 0; i < skews.length; i++) {
+			st = new Stream(length, skews[i]);
+			CMS sketch = get_counter(counter_name, depth, width);
+			double error = test_configuration(depth, width, length, skews[i], st, sketch);
+			System.out.println(counter_name + " Maximum error in stream is Skew : "  + 
+					String.valueOf(skews[i]) + " is  "+ String.valueOf(error));
+		}
+	}
+
+	public static double test_configuration(int depth, int width, int length, double skew, Stream st, CMS def) {
 		for (int i = 1; i <= length; i++) {
 			int item = st.stream[i];
 			def.update(item, 1);
-			conv.update(item, 1);
-			morris.update(item, 1);
 		}
 		double error = 0;
 		for (int i = 1; i < st.universe; i++) {
 		    int def_estimate = def.query(i);
-	            System.out.println(" DEFAULT: " + String.valueOf(def_estimate) + "  exact " + String.valueOf(st.get_exact(i)));
 		    if (st.get_exact(i) == 0) continue;
 		    double current_error = (def_estimate - st.get_exact(i))/(st.get_exact(i) + 0.0);
 		    if (current_error > error) {
+	                    //System.out.println(" Value: " + String.valueOf(def_estimate) + "  exact " + String.valueOf(st.get_exact(i)));
 			    error = current_error;
+	                    //System.out.println(" Error %: " + String.valueOf(error));
 		    }
-	            System.out.println(" Error %: " + String.valueOf(current_error));
 
 		}
 		return error;
--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 javac -classpath  stdlib.jar Morris.java Hash.java CMS.java CMS_default.java CMS_conservative.java CMS_Morris.java TestCMS.java Stream.java
 #java -cp stdlib.jar:./ TestCMS 100 1000 Stocks/processed/aame.us.txt.processed aame
 #java -cp stdlib.jar:./ TestCMS 32 200 aame.us.txt.processed query.file
-java -cp stdlib.jar:./ TestCMS 1 2000000 10000000 0.3
+java -cp stdlib.jar:./ TestCMS 1 800000 10000000 0.3
 echo "Running on all of the data"
 #java -cp stdlib.jar:./ TestCMS 1 200 data query.file
